56469e400b5ed16fffcba8acec2a8341e6ee5c5d
emmanuel <emmanuel@kawa-analytics.com>
2024-07-23 19:23:02 -0400

    incr

---
+++ toolkits/basic/length.py
@@ -0,0 +1,16 @@
+import logging
+import pandas as pd
+from kywy.client.kawa_decorators import outputs, inputs
+
+logger = logging.getLogger('script-logger')
+
+
+@inputs(text=str)
+@outputs(length1=int, length2=int)
+def execute(df: pd.DataFrame):
+    logger.info('Starting the execution')
+    
+    df['length1'] = df.apply(lambda row: len(row['text']), axis=1)
+    df['lenght2'] = df.apply(lambda row: len(row['text']), axis=1)
+    
+    return df
