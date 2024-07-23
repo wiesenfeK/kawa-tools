efb3a3faf279007da16e0c715108520ee53ebe4f
emmanuel <emmanuel@kawa-analytics.com>
2024-07-23 19:43:14 -0400

    incr

--- toolkits/basic/length.py
+++ toolkits/basic/length.py
@@ -6,11 +6,12 @@ logger = logging.getLogger('script-logger')
 
 
 @inputs(text=str)
-@outputs(length1=int, length2=int)
+@outputs(length1=int, length2=int, length3=int)
 def execute(df: pd.DataFrame):
     logger.info('Starting the execution')
     
     df['length1'] = df.apply(lambda row: len(row['text']), axis=1)
     df['lenght2'] = df.apply(lambda row: len(row['text']), axis=1)
+    df['length3'] = df.apply(lambda row: len(row['text']), axis=1)
     
     return df
